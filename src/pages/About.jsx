d82efe89f3bd274c9e4cf420d290153b910744fd
Darius <dariusford15@gmail.com>
2025-02-15 18:40:33 -0500

    fix image problem on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import portfolio_img from '../../src/assets/portfolio.jpg'
 
 function About() {
     const Education = [
@@ -28,7 +28,7 @@ function About() {
                 )}
                 <h1 className="pt-5 pb-5 text-3xl text-white text-center underline">About Me</h1>
                 <div class="box">
-                    <img src="/assets/portfolio.jpg" alt="" class="box-img" width="200px" height="300px" />
+                    <img src={portfolio_img} alt="" class="box-img" width="200px" height="300px" />
                         <h5>Web Developer - Web Designer</h5>
                         <p>I am a web developer, who enjoys creating websites that are appealing to the eye. I am a graduate of Colorado Technical University with a bachelors in Information Technology with a specialization in Web Development and also have a Full-Stack Web Development Certification.
                         My experience is shown through projects done through QuickStart. My main focus as of now is working with HTML, CSS, JavaScript and React, but I’m open to take on other technologies.
